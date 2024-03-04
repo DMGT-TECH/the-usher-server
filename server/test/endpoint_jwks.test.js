1f1a937a5cba49cbfc1f6a0277edeeaf44cbaf62
Keyvan Chamani <keyvan.chamani@gmail.com>
2024-03-04 15:38:13 -0500

    feat: topic/k1ch/ internal caching for GET:/jwks.json

--- server/test/endpoint_jwks.test.js
+++ server/test/endpoint_jwks.test.js
@@ -6,12 +6,19 @@ const { getServerUrl } = require('./lib/urls')
 
 describe('Get JWKS', () => {
   const url = `${getServerUrl()}/.well-known/jwks.json`
+  const fetchJWKS = () => fetch(url, { method: 'GET', headers: { 'Content-Type': 'application/json' } });
 
-  it('should return a JWKS containing a list of keys', async function () {
-    const response = await fetch(url, { method: 'GET', headers: { 'Content-Type': 'application/json' } })
-    assert.strictEqual(response.status, 200)
+  it('should return a JWKS containing a list of keys', async () => {
+    const response = await fetchJWKS()
+    assert.equal(response.status, 200)
     const jwks = await response.json()
     assert('keys' in jwks, 'There should be a keys element in the object.')
     assert(jwks.keys.length > 0, 'There should be at least one key in the keys array')
+  })
+
+  it('should return a JWKS from internal cache', async () => {
+    const response = await fetchJWKS()
+    assert.equal(response.status, 200)
+    assert.equal(response.headers.get('x-cache'), 'Hit from Usher')
   })
 })
