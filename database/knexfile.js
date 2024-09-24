9c8f6481bad3a9144ab9e6a18c263347cb41b5c4
Keyvan Chamani <keyvan.chamani@gmail.com>
2024-09-24 18:15:28 -0400

    chore: untracked/k1ch/introduce new knex envs

--- database/knexfile.js
+++ database/knexfile.js
@@ -10,13 +10,13 @@ module.exports = {
   },
   pool: {
     min: +process.env.KNEX_POOL_MIN || 0,
-    max: +process.env.KNEX_POOL_MAX || 5,
-    
+    max: +process.env.KNEX_POOL_MAX || 100,
+
     // tarn config (https://www.npmjs.com/package/tarn)
-    propagateCreateError: false,
-    createRetryIntervalMillis: 500,
-    createTimeoutMillis: 5000,
-    acquireTimeoutMillis: 5000,
-    reapIntervalMillis: 1000,
+    propagateCreateError: process.env.KNEX_POOL_PROPAGATE_CREATE_ERROR === 'true' || false,
+    createRetryIntervalMillis: +process.env.KNEX_POOL_CREATE_RETRY_INTERVAL_MILLIS || 500,
+    createTimeoutMillis: +process.env.KNEX_POOL_CREATE_TIMEOUT_MILLIS || 5000,
+    acquireTimeoutMillis: +process.env.KNEX_POOL_ACQUIRE_TIMEOUT_MILLIS || 5000,
+    reapIntervalMillis: +process.env.KNEX_POOL_REAP_INTERVAL_MILLIS || 1000,
   },
 }
