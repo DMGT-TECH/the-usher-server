b87c64c2eda5ce7e45cf4e3cba31a65bace9919c
Keyvan Chamani <keyvan.chamani@gmail.com>
2024-10-31 17:07:58 -0400

    feat: k1ch/ introduce POST:/clients/{client_id}/permissions

--- server/src/api_endpoints/clients/utils.js
+++ server/src/api_endpoints/clients/utils.js
@@ -1,8 +1,9 @@
 const dbAdminRole = require('database/layer/admin-client')
+const dbAdminPermission = require('database/layer/admin-permission')
 
 const checkClientExists = async (clientId) => {
   try {
-    await dbAdminRole.getClient(clientId);
+    return await dbAdminRole.getClient(clientId);
   } catch {
     throw {
       httpStatusCode: 404,
@@ -11,6 +12,32 @@ const checkClientExists = async (clientId) => {
   }
 }
 
+/**
+ * Checks the uniqueness of a permission name for a given client key.
+ *
+ * This function queries the database to retrieve permissions by name and client key.
+ * If any permissions are found, it throws an error indicating the name is already taken.
+ *
+ * @async
+ * @function checkPermissionNameUniqueness
+ * @param {Object} params - The parameters for checking uniqueness.
+ * @param {string} params.name - The name of the permission to check.
+ * @param {string} params.clientkey - The client key associated with the permission.
+ * @throws {Object} Throws an error with HTTP status code 409 if the permission name is not unique.
+ * @throws {number} error.httpStatusCode - The HTTP status code indicating conflict (409).
+ * @throws {string} error.message - The error message indicating the permission name is taken.
+ */
+const checkPermissionNameUniqueness = async ({ name, clientkey: clientKey }) => {
+  const permissions = await dbAdminPermission.getPermissionsByNameClientKey(name, clientKey);
+  if (permissions?.length) {
+    throw {
+      httpStatusCode: 409,
+      message: 'The permission name is taken!'
+    };
+  }
+};
+
 module.exports = {
   checkClientExists,
+  checkPermissionNameUniqueness,
 }
