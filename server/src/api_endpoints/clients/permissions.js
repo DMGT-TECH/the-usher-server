3b169f7047ef9e80d7ea22abce2f4a5286929e59
Keyvan Chamani <keyvan.chamani@gmail.com>
2024-12-20 16:52:56 -0500

    feat: topic/k1ch/ introduce API / GET/clients/{id}/permissions

--- server/src/api_endpoints/clients/permissions.js
+++ server/src/api_endpoints/clients/permissions.js
@@ -27,6 +27,27 @@ const createPermission = async (req, res, next) => {
   }
 }
 
+/**
+ * HTTP Request handler
+ * Get a list of permissions for a client
+ *
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object to send a 200 status code and the list of permissions
+ * @param {Function} next - The next middleware function
+ * @returns {Promise<void>} - A Promise that resolves to void when permissions are retrieved
+ */
+const getClientPermissions = async (req, res, next) => {
+  try {
+    const { client_id: clientId } = req.params
+    await checkClientExists(clientId)
+    const permissions = await dbAdminPermission.getPermissions({ clientId })
+    res.status(200).send(permissions)
+  } catch ({ httpStatusCode = 500, message }) {
+    return next(createError(httpStatusCode, { message }))
+  }
+}
+
 module.exports = {
   createPermission,
+  getClientPermissions,
 }
