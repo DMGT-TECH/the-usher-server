da2cc1f25a860f8ae723d62503596c92c0c5e12a
Keyvan Chamani <keyvan.chamani@gmail.com>
2025-01-03 17:36:51 -0500

    feat: topic / k1ch / introduce GET:/tenants

---
+++ server/src/api_endpoints/tenants/index.js
@@ -0,0 +1,33 @@
+const createError = require('http-errors')
+const dbAdminTenant = require('database/layer/admin-tenant')
+
+/**
+ * HTTP Request handler
+ * Get tenants with optional filtering, sorting, and ordering
+ *
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object to send 200 statusCode and a list of tenants
+ * @param {Function} next - The callback function for the next middleware
+ * @returns {Promise<void>} - A promise that resolves to void when tenants are retrieved
+ */
+const getTenants = async (req, res, next) => {
+  try {
+    const { sort, order } = req.query
+    const allowedFilterParameters = ['key', 'name', 'iss_claim']
+    const filters = allowedFilterParameters.reduce((acc, filter) => {
+      const filterValue = req.query[filter]
+      if (filterValue) {
+        acc[filter] = filterValue
+      }
+      return acc
+    }, {})
+    const tenants = await dbAdminTenant.getTenants(filters, sort, order)
+    res.status(200).send(tenants);
+  } catch ({ httpStatusCode = 500, message }) {
+    return next(createError(httpStatusCode, { message }))
+  }
+}
+
+module.exports = {
+  getTenants,
+}
